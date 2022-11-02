1ce8d324cb94620b153a657e87f1d0d2650616a4
saurav7036 <saurav.shashank12@gmail.com>
2022-11-02 10:54:44 +0530

    Add project files.

---
+++ BowlingBall/Games.cs
@@ -0,0 +1,101 @@
+﻿namespace BowlingBall
+{
+    public class Games
+    {
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+        private const int MAXROLL = 21;
+        public void Roll(int pins)
+        {
+            if (currentRoll < MAXROLL)
+            {
+                rolls[currentRoll] = pins;
+                currentRoll = currentRoll + 1;
+            }
+        }
+
+        /// <summary>
+        /// Gets total score
+        /// </summary>
+        /// <returns></returns>
+        public int GetTotalScore()
+        {
+            int score = 0;
+            currentRoll = 0;
+            for (var frames = 1; frames <= 10; frames++)
+            {
+                if (IsStrike(currentRoll))
+                {
+                    score = score + GetScoreByRoll(currentRoll) + Bonus(currentRoll);
+                    currentRoll = currentRoll + 1;
+                }
+                else if (IsSpare(currentRoll))
+                {
+                    score = score + GetScoreByRoll(currentRoll) + Bonus(currentRoll);
+                    currentRoll = currentRoll + 2;
+                }
+                else
+                {
+                    score = score + GetScoreByRoll(currentRoll);
+                    currentRoll = currentRoll + 1;
+
+                    score = score + GetScoreByRoll(currentRoll);
+                    currentRoll = currentRoll + 1;
+                }
+            }
+
+            return score;
+        }
+
+        /// <summary>
+        /// Check if current roll is a strike
+        /// </summary>
+        /// <param name="currentRoll"></param>
+        /// <returns></returns>
+        private bool IsStrike(int currentRoll)
+        {
+            if (rolls[currentRoll] == 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Check if current roll is a spare
+        /// </summary>
+        /// <param name="currentRoll"></param>
+        /// <returns></returns>
+        private bool IsSpare(int currentRoll)
+        {
+            if (rolls[currentRoll] + rolls[currentRoll + 1] == 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Calculates bonus
+        /// </summary>
+        /// <param name="currentRoll"></param>
+        /// <returns></returns>
+        private int Bonus(int currentRoll)
+        {
+            return rolls[currentRoll + 1] + rolls[currentRoll + 2];
+        }
+
+        /// <summary>
+        /// Gets score by roll
+        /// </summary>
+        /// <param name="currentRoll"></param>
+        /// <returns></returns>
+        private int GetScoreByRoll(int currentRoll)
+        {
+            return rolls[currentRoll];
+        }
+
+    }
+}
---
+++ BowlingBallTest/GamesFixture.cs
@@ -0,0 +1,59 @@
+﻿using BowlingBall;
+using Xunit;
+
+namespace BowlingBallTest
+{
+    public class GamesFixture
+    {
+        private Games game = new Games();
+
+        public GamesFixture()
+        {
+            game = new Games();
+        }
+
+        [Fact(DisplayName = " This is test will always pass.")]
+        public void DummyTest()
+        {
+
+        }
+
+        [Fact(DisplayName = "Get total score")]
+        public void HappyPath()
+        {
+            int[] score = { 10, 9, 1, 5, 5, 7, 2, 10, 10, 10, 9, 0, 8, 2, 9, 1, 10 };
+            for (var i = 0; i < score.Length; i++)
+            {
+                game.Roll(score[i]);
+            }
+
+            Assert.Equal(187, game.GetTotalScore());
+        }
+
+        [Fact(DisplayName = "Test to validate total score when each roll is a strike")]
+        public void AllStrikeTest()
+        {
+            for (var i = 0; i < 12; i++)
+                game.Roll(10);
+
+            Assert.Equal(300, game.GetTotalScore());
+        }
+
+        [Fact(DisplayName = "Test to validate total score when each roll is a spare")]
+        public void AllSpares()
+        {
+            for (var i = 0; i < 21; i++)
+                game.Roll(5);
+            Assert.Equal(150, game.GetTotalScore());
+        }
+
+        [Fact(DisplayName = "Test to validate total score when each roll is zero ")]
+        public void RollWithAllZero()
+        {
+            for (var i = 0; i < 20; i++)
+                game.Roll(0);
+            Assert.Equal(0, game.GetTotalScore());
+        }
+    }
+}
+
